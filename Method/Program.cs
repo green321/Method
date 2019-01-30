13e19126060dfb6bdc3467e54ca4801e4a41e21a
green321 <yuexu@mail.usf.edu>
2019-01-29 21:40:41 -0500

    - Method - private static return

--- Method/Program.cs
+++ Method/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+ Author : Yue Xu
+ Date   : 1/29/2019
+ Comment: 1. As the user to enter their name
+          2. Write a method that prints to the Console: "Hello users name!".
+          Here's an example of the User Input and Output:
+          INPUT: Clinton Daniel
+          OUTPUT: Hello Clinton Daniel!
+          Once you complete this program, upload it to GitHub.
+          Then, past in your GitHub Link to GitHub here.
+*/
+
+using System;
 
 namespace Method
 {
@@ -6,7 +18,45 @@ namespace Method
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("Please enter your name. ");
+
+            
+            try
+            {
+                // This variable will gather data from user input
+                string Input = Console.ReadLine();
+
+                // call method Recommend_Name
+                string get_your_name = Recommend_Name(Input);
+
+                Console.ReadKey(true);
+
+            
+
+            }//End of try
+
+            catch
+            {
+                Console.WriteLine("Please use the correctm name format");
+                Console.WriteLine("Press any key to exit the program and try again....");
+                Console.ReadKey(true);
+            }// End of catch
+
+ 
+        }//End of main
+
+         //static = can not dupulicate 
+         //ing = going out with int value
+         //Recommne_xxx= int(get _age) = come in and you got a new name get_age
+
+        private static string Recommend_Name(string get_name)
+        {
+            string name_you_entered = get_name;
+            Console.WriteLine("Hello " + get_name + " !");
+
+            return name_you_entered;
+
+        }// End of Recommend_Name
+    }//End of class
+
+}//End of namespace
